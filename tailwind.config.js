076d9953b7c9e3c63e5b82aa13e54bea496a5c38
Raphael Awoyinfa <raphicogit@gmail.com>
2023-11-03 13:44:00 -0700

    added styling configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--input))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        muted: "hsl(var(--muted))",
+        pending: "hsl(var(--pending))",
+        paid: "hsl(var(--paid))",
+        draft: "hsl(var(--draft))",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        edit: {
+          DEFAULT: "hsl(var(--edit))",
+          foreground: "hsl(var(--edit-foreground))",
+        },
+        delete: {
+          DEFAULT: "hsl(var(--delete))",
+          foreground: "hsl(var(--delete-foreground))",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) -2px)",
+      },
+    },
   },
   plugins: [],
 }
