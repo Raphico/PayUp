f71184f9f1fec38385f62894e9b8655995260b1e
Raphael Awoyinfa <raphicogit@gmail.com>
2023-11-04 13:50:56 -0700

    refactored theme toggler to improve readability

--- src/components/layouts/ThemeToggler.tsx
+++ src/components/layouts/ThemeToggler.tsx
@@ -4,10 +4,11 @@ import { SunIcon, MoonIcon } from "@radix-ui/react-icons"
 
 export function ThemeToggler() {
   const { theme, toggleTheme } = useTheme()
+  const isDarkTheme = theme === "dark"
 
   return (
     <Button variant="ghost" sizes="icon" onClick={toggleTheme}>
-      {theme === "dark" ? (
+      {isDarkTheme ? (
         <SunIcon className="h-6 w-6" aria-hidden="true" />
       ) : (
         <MoonIcon className="h-6 w-6" aria-hidden="true" />
