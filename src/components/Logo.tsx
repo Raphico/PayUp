3fe4bde9df7b065a20298ad4bf422622db70fae2
Raphael Awoyinfa <raphicogit@gmail.com>
2023-11-04 13:11:24 -0700

    changed logo styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div className="flex items-center gap-2">
       <img src="/logo.svg" alt="PayUp logo" width={30} height={30} />
-      <h3 className="heading3">PayUp</h3>
+      <h3 className="heading3 max-sm:hidden">PayUp</h3>
     </div>
   )
 }
