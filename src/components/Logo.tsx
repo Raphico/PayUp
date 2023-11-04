45344f1ef626429a3a3c79a69cdd9379fe2fd81b
Raphael Awoyinfa <raphicogit@gmail.com>
2023-11-04 12:24:44 -0700

    added logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <div className="flex items-center gap-2">
+      <img src="/logo.svg" alt="PayUp logo" width={30} height={30} />
+      <h3 className="heading3">PayUp</h3>
+    </div>
+  )
+}
