a235206cde96ebcdca4d9ce3b52911cfa49d31ed
Raphael Awoyinfa <raphicogit@gmail.com>
2023-11-08 06:18:31 -0800

    added page header component

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { cn } from "../lib/utils"
+
+interface PageHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  as?: React.ElementType
+}
+
+const PageHeader = ({
+  className,
+  as: Comp = "section",
+  ...props
+}: PageHeaderProps) => (
+  <Comp className={cn("grid gap-1", className)} {...props} />
+)
+
+interface PageHeaderHeadingProps
+  extends React.HTMLAttributes<HTMLHeadingElement> {}
+
+const PageHeaderHeading = ({ className, ...props }: PageHeaderHeadingProps) => (
+  <h1
+    className={cn(
+      "font-bold leading-tight tracking-tighter lg:leading-[1.1] text-2xl",
+      className
+    )}
+    {...props}
+  />
+)
+
+interface PageHeaderDescriptionProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {}
+
+const PageHeaderDescription = ({
+  className,
+  ...props
+}: PageHeaderDescriptionProps) => (
+  <p className={cn("font-medium text-base text-muted", className)} {...props} />
+)
+
+export { PageHeader, PageHeaderHeading, PageHeaderDescription }
