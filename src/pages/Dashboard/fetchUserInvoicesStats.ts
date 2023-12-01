2520349229ee374c3f420aab711dc8b1e73b1f51
Raphael Awoyinfa <raphicogit@gmail.com>
2023-12-01 14:49:54 -0800

    refactored fetchUserInvoicesStats to handle errors

--- src/pages/Dashboard/fetchUserInvoicesStats.ts
+++ src/pages/Dashboard/fetchUserInvoicesStats.ts
@@ -1,47 +1,58 @@
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../../lib/firebase"
 import { Invoice } from "../../types"
+import { FirebaseError } from "firebase/app"
+import { formatCurrency } from "../../lib/utils"
 
 export const fetchUserInvoicesStats = async (uid: string) => {
-  const invoicesRef = collection(db, "invoices")
+  try {
+    const invoicesRef = collection(db, "invoices")
 
-  const userInvoicesQuery = query(invoicesRef, where("uid", "==", uid))
-  const querySnapshot = await getDocs(userInvoicesQuery)
+    const userInvoicesQuery = query(invoicesRef, where("uid", "==", uid))
+    const querySnapshot = await getDocs(userInvoicesQuery)
 
-  const totalInvoiceCount = querySnapshot.size
+    const totalInvoiceCount = querySnapshot.size
 
-  let totalInvoicesAmount = 0
-  let pendingInvoicesCount = 0
-  let draftedInvoicesCount = 0
-  let paidInvoicesCount = 0
+    let totalInvoicesAmount = 0
+    let pendingInvoicesCount = 0
+    let draftedInvoicesCount = 0
+    let paidInvoicesCount = 0
 
-  querySnapshot.docs.forEach((doc) => {
-    const invoice = doc.data() as Invoice
+    querySnapshot.docs.forEach((doc) => {
+      const invoice = doc.data() as Invoice
 
-    totalInvoicesAmount += invoice.amount
+      totalInvoicesAmount += invoice.amount
 
-    switch (invoice.invoiceStatus) {
-      case "pending":
-        pendingInvoicesCount++
-        break
-      case "paid":
-        paidInvoicesCount++
-        break
-      default:
-        draftedInvoicesCount++
-        break
+      switch (invoice.invoiceStatus) {
+        case "pending":
+          pendingInvoicesCount++
+          break
+        case "paid":
+          paidInvoicesCount++
+          break
+        default:
+          draftedInvoicesCount++
+          break
+      }
+    })
+
+    const averageInvoiceAmount = totalInvoicesAmount
+      ? totalInvoicesAmount / totalInvoiceCount
+      : 0
+
+    return {
+      totalInvoiceCount,
+      totalInvoicesAmount,
+      averageInvoiceAmount,
+      pendingInvoicesCount,
+      draftedInvoicesCount,
+      paidInvoicesCount,
     }
-  })
+  } catch (error) {
+    error instanceof FirebaseError
+      ? console.error(error.message)
+      : console.error(error)
 
-  const averageInvoiceAmount =
-    totalInvoicesAmount && totalInvoicesAmount / totalInvoiceCount
-
-  return {
-    totalInvoiceCount,
-    totalInvoicesAmount,
-    averageInvoiceAmount,
-    pendingInvoicesCount,
-    draftedInvoicesCount,
-    paidInvoicesCount,
+    throw new Error("Unable to fetch user invoice stats")
   }
 }
