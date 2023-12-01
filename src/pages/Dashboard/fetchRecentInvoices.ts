476c491e9533e3681492462ce76cf352f0b914fe
Raphael Awoyinfa <raphicogit@gmail.com>
2023-12-01 14:36:08 -0800

    refactored fetchRecentInvoices to handle errors

--- src/pages/Dashboard/fetchRecentInvoices.ts
+++ src/pages/Dashboard/fetchRecentInvoices.ts
@@ -8,19 +8,28 @@ import {
 } from "firebase/firestore"
 import { db } from "../../lib/firebase"
 import { Invoice } from "../../types"
+import { FirebaseError } from "firebase/app"
 
 export const fetchRecentInvoices = async (uid: string) => {
-  const invoicesRef = collection(db, "invoices")
+  try {
+    const invoicesRef = collection(db, "invoices")
 
-  const userInvoicesQuery = query(
-    invoicesRef,
-    where("uid", "==", uid),
-    orderBy("invoiceDate", "desc"),
-    limit(5)
-  )
-  const querySnapshot = await getDocs(userInvoicesQuery)
+    const userInvoicesQuery = query(
+      invoicesRef,
+      where("uid", "==", uid),
+      orderBy("invoiceDate", "desc"),
+      limit(5)
+    )
+    const querySnapshot = await getDocs(userInvoicesQuery)
 
-  return querySnapshot.docs.map(
-    (doc) => ({ ...doc.data(), id: doc.id } as Invoice)
-  )
+    return querySnapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id } as Invoice)
+    )
+  } catch (error) {
+    error instanceof FirebaseError
+      ? console.error(error.message)
+      : console.error(error)
+
+    throw new Error("Unable to fetch recent invoices")
+  }
 }
