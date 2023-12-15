cbbc110e07901e01056fc2bfbd59ed1612d619a6
Raphael Awoyinfa <raphicogit@gmail.com>
2023-12-15 07:42:05 -0800

    Enhanced GoBack component to handle specific redirects

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,14 +1,29 @@
 import { Button } from "./ui/Button"
 import { ChevronLeftIcon } from "@radix-ui/react-icons"
-import { useNavigate } from "react-router-dom"
+
+import { useNavigate, useLocation } from "react-router-dom"
 
 export function GoBack() {
   const navigate = useNavigate()
+  const location = useLocation()
 
-  const PREVIOUS_PAGE = -1
+  const handleGoBack = () => {
+    // If the current path is "/invoice/:id", always redirect to "/invoice"
+    if (location.pathname.startsWith("/invoice/")) {
+      navigate("/invoices")
+    }
+    // If the current path is "/sign-in", always redirect to "/"
+    else if (location.pathname === "/sign-in") {
+      navigate("/")
+    }
+    // Otherwise, use the default behavior to go back to the previous page
+    else {
+      navigate(-1)
+    }
+  }
 
   return (
-    <Button onClick={() => navigate(PREVIOUS_PAGE)} variant="ghost">
+    <Button onClick={handleGoBack} variant="ghost">
       <ChevronLeftIcon className="h-4 w-4 mr-2" aria-hidden="true" />
       Go back
     </Button>
