3172ba8ed7b16616f807fca7a6c3178e1da1bb51
Raphael Awoyinfa <raphicogit@gmail.com>
2023-12-15 06:10:29 -0800

    removed unused imports

--- src/pages/SignIn/OAuthSignIn.tsx
+++ src/pages/SignIn/OAuthSignIn.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { useNavigate } from "react-router-dom"
 import { OAuthStrategy, useAuth } from "../../contexts/AuthContext"
 import { Button } from "../../components/ui/Button"
 import { Icons } from "../../components/Icons"
