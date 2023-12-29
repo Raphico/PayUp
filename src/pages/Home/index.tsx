224fa27eb7fdb1202ec6bb8e9ad3c86b45476351
Raphael Awoyinfa <raphicogit@gmail.com>
2023-12-29 02:53:06 -0800

    updated home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,15 +12,6 @@ export function HomePage() {
       <AuthNav />
       <main className="max-width y-paddings space-y-20">
         <section className="hero">
-          <Link
-            to="https://github.com/Raphico/payUp"
-            target="_blank"
-            className={cn(buttonVariants({ variant: "outline" }))}
-          >
-            <Icons.github className="h-4 w-4 mr-2" aria-hidden="true" />
-            Github
-          </Link>
-
           <h1 className="hero-heading">
             A powerful tool for managing and creating invoices efficiently
           </h1>
@@ -28,16 +19,95 @@ export function HomePage() {
             Simplify your invoicing and billing with Your Invoice App. Manage
             your finances with ease.
           </p>
-          <Link to="/sign-in" className={buttonVariants({ variant: "accent" })}>
-            Get started
-          </Link>
+          <div className="flex items-center gap-4">
+            <Link
+              to="/sign-in"
+              className={buttonVariants({ variant: "accent" })}
+            >
+              Get started
+            </Link>
+            <Link
+              to="https://github.com/Raphico/payUp"
+              target="_blank"
+              className={cn(buttonVariants({ variant: "outline" }))}
+            >
+              <Icons.github className="h-4 w-4 mr-2" aria-hidden="true" />
+              Github
+            </Link>
+          </div>
         </section>
 
         <img src="/dashboard.png" alt="PayUp dashboard page" />
 
         <section className="flex-center flex-col gap-8">
           <h2 className="text-center text-2xl sm:text-3xl font-semibold">
-            Features
+            Why PayUp?
+          </h2>
+
+          <div className="grid gap-4 sm:grid-cols-2 max-w-4xl">
+            <Card>
+              <CardContent className="space-y-2 p-8">
+                <Icons.invoices
+                  className="h-10 w-10 text-foreground"
+                  aria-hidden="true"
+                />
+                <h3 className="text-lg font-semibold">
+                  Intuitive Invoice Creation
+                </h3>
+                <p className="text-muted">
+                  Effortlessly generate professional invoices with a
+                  user-friendly interface
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent className="space-y-2 p-8">
+                <Icons.dashboard
+                  className="h-10 w-10 text-white"
+                  aria-hidden="true"
+                />
+                <h3 className="text-lg font-semibold">Dashboard Overview</h3>
+                <p className="text-muted">
+                  Monitor key metrics and gain insights into your financial
+                  performance at a glance
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent className="space-y-2 p-8">
+                <Icons.interface
+                  className="h-10 w-10 text-foreground"
+                  aria-hidden="true"
+                />
+                <h3 className="text-lg font-semibold">
+                  User-Friendly Interface
+                </h3>
+                <p className="text-muted">
+                  Save time and reduce complexity with an intuitive design
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent className="space-y-2 p-8">
+                <Icons.table
+                  className="h-10 w-10 text-foreground"
+                  aria-hidden="true"
+                />
+                <h3 className="text-lg font-semibold">
+                  Dynamic Invoice Management
+                </h3>
+                <p className="text-muted">
+                  Effortlessly manage invoices with a dynamic table that tailors
+                  to your need
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </section>
+
+        <section className="flex-center flex-col gap-8">
+          <h2 className="text-center text-2xl sm:text-3xl font-semibold">
+            Under the Hood
           </h2>
 
           <div className="grid gap-4 sm:grid-cols-2 max-w-4xl">
@@ -49,7 +119,7 @@ export function HomePage() {
                 />
                 <h3 className="text-lg font-semibold">React</h3>
                 <p className="text-muted">
-                  Client component, Context API, hooks, Code splitting.
+                  Client component, Context API, hooks, Code splitting
                 </p>
               </CardContent>
             </Card>
@@ -63,7 +133,8 @@ export function HomePage() {
                   Firebase & Tanstack Query
                 </h3>
                 <p className="text-muted">
-                  User, Data & Asynchronous state management
+                  User & Data Management with firebase with react query for
+                  handling data fetching and mutations
                 </p>
               </CardContent>
             </Card>
